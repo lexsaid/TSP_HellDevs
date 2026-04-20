af07d1857ce36d45d6867c590993aa5d4bfe6078
EldiegoGDL <diegoner.dg@gmail.com>
2026-04-20 15:06:18 -0600

    cambiamos los modelos del la app pasada de GO a Python

---
+++ backend_Python/app/modelos/Imagen.py
@@ -0,0 +1,12 @@
+class Imagen:
+    def __init__(self, id_imagen, id_trabajo, imagen):
+        self.id_imagen = id_imagen
+        self.id_trabajo = id_trabajo
+        self.imagen = imagen  # bytes
+
+    def to_dict(self):
+        return {
+            "id_imagen": self.id_imagen,
+            "id_trabajo": self.id_trabajo,
+            "imagen": self.imagen
+        }
---
+++ backend_Python/app/modelos/Trabajo.py
@@ -0,0 +1,35 @@
+class Trabajo:
+    def __init__(
+        self,
+        id_trabajo,
+        nombre,
+        ubicacion,
+        fecha_publicacion,
+        monto,
+        descripcion,
+        id_usuario,
+        tipo_trabajo,
+        estado
+    ):
+        self.id_trabajo = id_trabajo
+        self.nombre = nombre
+        self.ubicacion = ubicacion
+        self.fecha_publicacion = fecha_publicacion
+        self.monto = monto
+        self.descripcion = descripcion
+        self.id_usuario = id_usuario
+        self.tipo_trabajo = tipo_trabajo
+        self.estado = estado
+
+    def to_dict(self):
+        return {
+            "id_trabajo": self.id_trabajo,
+            "nombre": self.nombre,
+            "ubicacion": self.ubicacion,
+            "fecha_publicacion": self.fecha_publicacion,
+            "monto": self.monto,
+            "descripcion": self.descripcion,
+            "id_usuario": self.id_usuario,
+            "tipo_trabajo": self.tipo_trabajo,
+            "estado": self.estado
+        }
